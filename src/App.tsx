b1f5669a6f58344d9400b7ea35ae01c9c0df244c
Arthur Theodoro <arthur.theodoro.sva@hotmail.com>
2023-05-01 23:13:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// Styles
+import { Container } from "./styles/container"
+
+function App() {
+  return (
+    <Container>
+
+      
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/layout/navbar/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    width:100%;
+    padding:2em .5em;
+
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    
+`
+
+export const Title = styled.div`
+    margin:0 15px;
+
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+
+    width:150px;
+`
+
+export const NavLinks = styled.div`
+    
+`
